59791c32721348e437b4c5eaaa8851f1eab3fb73
Michael Skvortsov <miskvortsov@edu.hse.ru>
2026-07-17 18:54:21 +0300

    Ok

--- backend/Eido_quiz/settings.py
+++ backend/Eido_quiz/settings.py
@@ -38,13 +38,9 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-CORS_ALLOWED_ORIGINS = [
-    os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:5173').split(','),
-]
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:5173').split(','),
 
-CSRF_TRUSTED_ORIGINS = [
-    os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:5173').split(','),
-]
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:5173').split(','),
 
 
 # Application definition
